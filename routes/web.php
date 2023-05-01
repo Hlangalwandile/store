7c935d6f54ebe2e407a482f5ed80f0ce302cfcec
Hlanga <hlanga.bani@gmail.com>
2023-05-01 17:42:27 +0200

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes([
+    'verify'=> true
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('products')->controller(ProductController::class)->group(function(){
+    Route::get('/','index')->name('products');
+    Route::get('/product','show')->name('product.show');
+    Route::get('/create','create')->name('product.create');
+    Route::get('/edit','edit')->name('product.edit');
+});
+
+Route::prefix('categories')->controller(CategoryController::class)->group(function(){
+    Route::get('/','index')->name('categories');
+    Route::get('/category','show')->name('category.show');
+    Route::get('/create','create')->name('category.create');
+    Route::get('/edit','edit')->name('category.edit');
 });
